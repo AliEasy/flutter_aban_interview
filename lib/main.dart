d1177a903988321017c646469202ee473f7ebd50
Ali <ali.ezad77@gmail.com>
2025-04-24 11:51:36 +0330

    add di_setup for initializing injectable and call it inside setup.dart - call setup.dart before runapp in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_aban_interview/flutter_aban_interview.dart';
+import 'package:flutter_aban_interview/setup.dart';
 
 void main() {
+  setupApp();
   runApp(const FlutterAbanInterview());
 }
