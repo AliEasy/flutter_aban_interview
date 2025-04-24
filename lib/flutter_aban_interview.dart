5ba0832a16b6b9d006b2525e84a35ac319d1b4e6
Ali <ali.ezad77@gmail.com>
2025-04-24 22:51:44 +0330

    setup theme_management_cubit.dart on MaterialTheme(entry point) so that user can change it manually

--- lib/flutter_aban_interview.dart
+++ lib/flutter_aban_interview.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aban_interview/core/di/base/di_setup.dart';
+import 'package:flutter_aban_interview/core/theme/management/theme_management_cubit.dart';
 import 'package:flutter_aban_interview/features/login/presentation/page/login_page.dart';
 import 'package:flutter_aban_interview/generated/l10n.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 class FlutterAbanInterview extends StatelessWidget {
@@ -8,39 +11,51 @@ class FlutterAbanInterview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Aban Interview',
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: const [Locale('en')],
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        useMaterial3: true,
-        colorSchemeSeed: const Color(0xFF4C6FFF),
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-        ),
-        dialogTheme: const DialogTheme(
-          titleTextStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
+    return BlocProvider(
+      create: (context) => getIt<ThemeManagementCubit>(),
+      child: BlocBuilder<ThemeManagementCubit, ThemeMode>(
+        builder: (context, state) {
+          return Builder(
+            builder: (context) {
+              return MaterialApp(
+                title: 'Aban Interview',
+                localizationsDelegates: const [
+                  S.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                ],
+                supportedLocales: const [Locale('en')],
+                debugShowCheckedModeBanner: false,
+                themeMode: state,
+                theme: ThemeData(
+                  brightness: Brightness.light,
+                  useMaterial3: true,
+                  colorSchemeSeed: const Color(0xFF4C6FFF),
+                  appBarTheme: const AppBarTheme(
+                    centerTitle: true,
+                  ),
+                  dialogTheme: const DialogTheme(
+                    titleTextStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                darkTheme: ThemeData(
+                  brightness: Brightness.dark,
+                  useMaterial3: true,
+                  colorSchemeSeed: const Color(0xFFFF7847),
+                  appBarTheme: const AppBarTheme(
+                    centerTitle: true,
+                  ),
+                  dialogTheme: const DialogTheme(
+                    titleTextStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                home: const LoginPage(),
+              );
+            }
+          );
+        },
       ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-        colorSchemeSeed: const Color(0xFFFF7847),
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-        ),
-        dialogTheme: const DialogTheme(
-          titleTextStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-      ),
-      home: const LoginPage(),
     );
   }
 }
