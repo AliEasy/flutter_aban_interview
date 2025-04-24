d3992ada919254076bad8bc4983c954e5e572545
Ali <ali.ezad77@gmail.com>
2025-04-24 21:57:38 +0330

    add update_phone_number_form_validator_cubit and state to validate input value by user and if its valid enable submit button

---
+++ lib/features/profile/presentation/manager/update_phone_number_form_validator/update_phone_number_form_validator_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_aban_interview/core/common/constants/regex.dart';
+import 'package:flutter_aban_interview/generated/l10n.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+part 'update_phone_number_form_validator_state.dart';
+
+part 'update_phone_number_form_validator_cubit.freezed.dart';
+
+@injectable
+class UpdatePhoneNumberFormValidatorCubit
+    extends Cubit<UpdatePhoneNumberFormValidatorState> {
+  UpdatePhoneNumberFormValidatorCubit()
+      : super(const UpdatePhoneNumberFormValidatorState.invalid());
+
+  String? validateEmail(String phoneNumber) {
+    final isValid = RegExp(iranMobileNumberRegex).hasMatch('+98$phoneNumber');
+    if (isValid) {
+      emit(const UpdatePhoneNumberFormValidatorState.valid());
+      return null;
+    } else {
+      emit(const UpdatePhoneNumberFormValidatorState.invalid());
+      return S.current.phone_number_is_not_valid;
+    }
+  }
+}
---
+++ lib/features/profile/presentation/manager/update_phone_number_form_validator/update_phone_number_form_validator_state.dart
@@ -0,0 +1,7 @@
+part of 'update_phone_number_form_validator_cubit.dart';
+
+@freezed
+class UpdatePhoneNumberFormValidatorState with _$UpdatePhoneNumberFormValidatorState {
+  const factory UpdatePhoneNumberFormValidatorState.invalid() = _Invalid;
+  const factory UpdatePhoneNumberFormValidatorState.valid() = _valid;
+}
