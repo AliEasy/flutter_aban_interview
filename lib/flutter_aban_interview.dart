459f36960d3f80cf1a8ad5e3f2592a88429ffd61
Ali <ali.ezad77@gmail.com>
2025-04-24 11:45:36 +0330

    modify main.dart and separate app widget - set initial page to LoginPage - set localization to en - add light theme and dark theme

---
+++ lib/flutter_aban_interview.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_aban_interview/features/login/presentation/page/login_page.dart';
+import 'package:flutter_aban_interview/generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class FlutterAbanInterview extends StatelessWidget {
+  const FlutterAbanInterview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aban Interview',
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en')],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFF4C6FFF),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFFFF7847),
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
